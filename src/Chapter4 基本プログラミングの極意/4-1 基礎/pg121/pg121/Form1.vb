de041221ac37366fedde785688384616d411bba8
Tomoaki Masuda <masuda@moonmile.net>
2020-06-29 11:14:55 +0900

    フォルダー整理

---
+++ src/Chapter4 基本プログラミングの極意/4-1 基礎/pg121/pg121/Form1.vb
@@ -0,0 +1,20 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) _
+        Handles Button1.Click
+        Dim i As Integer
+
+        ' 行頭に 「'」を記述する
+        i = 100 * 2 ' 行の途中からもコメントにできます
+        ' 次の行は行頭に「'」があるので実行されません
+        ' i += 100
+
+        '
+        ' 複数コメントの例です
+        '
+        MessageBox.Show($"i = {i}", "確認")
+
+        Dim ch As Integer = Asc("A")
+        Dim xx(10)()
+
+    End Sub
+End Class
